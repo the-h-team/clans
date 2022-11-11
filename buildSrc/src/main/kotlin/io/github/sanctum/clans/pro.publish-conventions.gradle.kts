30966f570583d4bb2c286dc1df7f70a3094bdce2
Matt <58913953+ms5984@users.noreply.github.com>
2022-11-11 04:05:59 -0500

    Publishing setup * Move publishing into afterEvaluate to allow for late resolution of project properties * Fill in pom metadata * TODOs: check inceptionYear, think about license>distribution override for API, configure signing

--- buildSrc/src/main/kotlin/io/github/sanctum/clans/pro.publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/sanctum/clans/pro.publish-conventions.gradle.kts
@@ -3,8 +3,45 @@ plugins {
     `maven-publish`
 }
 
-publishing {
-    publications.create<MavenPublication>(name) {
-        from(components["java"])
+afterEvaluate {
+    publishing {
+        publications.create<MavenPublication>(name) {
+            pom {
+                name.set("ClansPro")
+                description.set(project.description!!)
+                url.set(project.properties["url"] as String)
+                inceptionYear.set(project.properties["inceptionYear"] as String)
+                organization {
+                    name.set("Sanctum")
+                    url.set("https://github.com/the-h-team")
+                }
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                        distribution.set("manual") // TODO: override for API
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ms5984")
+                        name.set("Matt")
+                        url.set("https://github.com/ms5984")
+                    }
+                    developer {
+                        id.set("Hempfest")
+                        name.set("Austin")
+                        url.set("https://github.com/Hempfest")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/the-h-team/ClansPro.git")
+                    developerConnection.set("scm:git:ssh://github.com/the-h-team/ClansPro.git")
+                    url.set("https://github.com/the-h-team/ClansPro/tree/main")
+                }
+            }
+            // TODO signing plugin
+            from(components["java"])
+        }
     }
 }
