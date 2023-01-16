a0cca7f073c832854057b1a090f4467ac94b0064
Matt <58913953+ms5984@users.noreply.github.com>
2023-01-16 14:01:35 -0500

    Pull out REST; continue work on models

--- api/src/main/java/io/github/sanctum/clans/api/model/Associate.java
+++ api/src/main/java/io/github/sanctum/clans/api/model/Associate.java
@@ -2,6 +2,8 @@ package io.github.sanctum.clans.api.model;
 
 import org.intellij.lang.annotations.Pattern;
 import org.intellij.lang.annotations.RegExp;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents a member of a clan.
@@ -25,4 +27,46 @@ public interface Associate {
      */
     @Pattern(NAME_FORMAT)
     @interface Name {}
+
+    /**
+     * Represents different types of membership updating.
+     *
+     * @since 3.0.0
+     */
+    enum MemberUpdateType {
+        /**
+         * The associate is joining a clan.
+         */
+        JOIN,
+        /**
+         * The associate is leaving a clan.
+         */
+        LEAVE,
+        /**
+         * The associate is being kicked from a clan.
+         */
+        KICK,
+        /**
+         * The associate is being removed from a clan administratively.
+         */
+        REMOVE,
+        /**
+         * The associate is being added to a clan administratively.
+         */
+        SET,
+    }
+
+    /**
+     * Gets the name of this associate.
+     *
+     * @return the name of this associate
+     */
+    @NotNull @Associate.Name String getName();
+
+    /**
+     * Gets the clan of which this associate is a member, if any.
+     *
+     * @return the clan this associate is in or null
+     */
+    @Nullable Clan getClan();
 }
