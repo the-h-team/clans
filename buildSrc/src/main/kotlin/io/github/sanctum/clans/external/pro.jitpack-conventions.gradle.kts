effb7c2de3ac48b409915b51be5d9361c66f0e65
Matt <58913953+ms5984@users.noreply.github.com>
2023-01-17 00:45:02 -0500

    Add nickname; draft Clan api * Add Documented to meta-annotations so that they show up properly (oops!) * Set up Bio.Target, Nickname and Nickname.Target to cleanly define superinterface for Clan * Pull in new panther snapshot via Sonatype; pull in labyrinth-loci for API (as `implementation`, we'll see if it works when consumed; might need to be listed as `api`)

--- buildSrc/src/main/kotlin/io/github/sanctum/clans/external/pro.jitpack-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/sanctum/clans/external/pro.jitpack-conventions.gradle.kts
@@ -5,7 +5,6 @@ repositories {
             // Try to explicitly select content
             includeModule("com.github.the-h-team", "Enterprise")
             includeModule("com.github.webbukkit", "dynmap-api")
-            includeGroup("com.github.the-h-team.Panther")
             includeGroup("com.github.the-h-team.Labyrinth")
 //            // Backup rule
 //            includeGroupByRegex("com\\.github\\..+") // only include com.github.* groups
