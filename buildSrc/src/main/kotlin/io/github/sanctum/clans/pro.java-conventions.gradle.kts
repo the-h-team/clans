01b00ef7f03dcd1145071931788ac41737e2ed37
Matt <58913953+ms5984@users.noreply.github.com>
2023-01-18 13:13:26 -0500

    Fix build; flush stash * Use s01 coordinates (as I had removed Panther group from jitpack-conventions) * Resolve property from convention * From stash: Move javadoc+sources jar config to publish-conventions

--- buildSrc/src/main/kotlin/io/github/sanctum/clans/pro.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/sanctum/clans/pro.java-conventions.gradle.kts
@@ -14,18 +14,10 @@ dependencies {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
-    withJavadocJar()
-    withSourcesJar()
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-}
-
-tasks.withType<Javadoc> {
-    options.encoding = "UTF-8"
-    (options as StandardJavadocDocletOptions).addBooleanOption("Xdoclint:reference", true)
-    options.quiet()
 }
 
 tasks.withType<Test> {
